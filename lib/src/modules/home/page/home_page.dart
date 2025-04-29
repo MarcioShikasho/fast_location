4ebeb71b71dd44e30e220a044712bc7308cb393e
Marcio <mksdeveloper@hotmail.com>
2025-04-29 09:27:45 -0300

    primeiro commit

---
+++ lib/src/modules/home/page/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:fast_location/src/shared/colors/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: AppColors.appPageBackground,
+      body: Text('Home Page'),
+    );
+  }
+}
